bb45c684dd037eccfa1af428777feaccb972cc80
biraj-dahal <dahalbiraj10@gmail.com>
2025-02-11 20:46:09 -0500

    Initial Commit

---
+++ Memory Cards/Memory_CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_CardsApp.swift
+//  Memory Cards
+//
+//  Created by Biraj Dahal on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Memory_CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
